2618a98c592c12ca8c2ce73b101bab54b94efdf6
Lukáš Nezbeda <240403@vut.cz>
2026-04-19 18:41:31 +0200

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # run experiments
-python -u train/deeplabv3_plus/train_deeplabv3_plus.py > log.txt
+python -u train/deeplabv3_plus/train_deeplabv3_plus.py --dataset penn_fudan > log.txt
 
