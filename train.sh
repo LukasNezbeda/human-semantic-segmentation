9d9a4616e807dd56a32b5813a930fa2cebf2a1f8
Lukáš Nezbeda <240403@vut.cz>
2026-05-04 18:54:41 +0200

    fix(train): training segf option

--- train.sh
+++ train.sh
@@ -3,5 +3,6 @@
 # run experiments
 # python -u train/train_dl3p_pseg.py > log.txt
 # python -u train/train_dl3p_pennfud.py > log.txt
-python -u train/train_dl3p_city.py > log.txt
+# python -u train/train_dl3p_city.py > log.txt
+python -u train/train_segf_city.py > log.txt
 
