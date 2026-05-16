ef51ee5b8ea49fa00517993a53b7a0097416a970
Lukáš Nezbeda <240403@vut.cz>
2026-05-16 17:32:45 +0200

    Update train.sh

--- train.sh
+++ train.sh
@@ -5,6 +5,6 @@
 # python -u train/train_dl3p_pennfud.py > log.txt
 # python -u train/train_dl3p_city.py > log.txt
 # python -u train/train_segf_city.py > log.txt
-python -u train/train_segnx_city.py > log.txt
-# python -u train/train_unet3p_city.py > log.txt
+# python -u train/train_segnx_city.py > log.txt
+python -u train/train_unet3p_city.py > log.txt
 
