526e3d1eb07474a9c9ddfee12f76c54c529075b9
Lukáš Nezbeda <240403@vut.cz>
2026-05-28 11:46:01 +0200

    Update train_all.sh

--- train_all.sh
+++ train_all.sh
@@ -5,9 +5,9 @@ set -euo pipefail
 # cd /path/to/your/project
 
 SCRIPTS=(
-  "train_dl3p.sh"
-  "train_segf.sh"
-  "train_segnx.sh"
+  # "train_dl3p.sh"
+  # "train_segf.sh"
+  # "train_segnx.sh"
   "train_unet3p.sh"
 )
 
