b82e2c3794550a78231d218e58df55c84ddc4492
Lukáš Nezbeda <240403@vut.cz>
2026-05-29 18:30:54 +0200

    Update eval_dl3p_city.py

--- eval/eval_dl3p_city.py
+++ eval/eval_dl3p_city.py
@@ -39,24 +39,24 @@ from train.train_dl3p_city import load_data
 # 3) Loading Data
 
 """ Global parameters """
-# H = 512
-# W = 1024
+H = 512
+W = 1024
 ZERO_DIVISION = 0
 
 """ Global params (Penn Fudan)"""
-H = 512
-W = 512
+# H = 512
+# W = 512
 
 # Get the project root directory (parent of the train folder)
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Cityscapes Testing
 model_path = os.path.join(project_root, "runs", "dl3p_city", "deeplabv3_plus.h5")
-# results_root = os.path.join(project_root, "results", "dl3p_city")
+results_root = os.path.join(project_root, "results", "dl3p_city")
 
 # Pennfudan Testing
 # model_path = os.path.join(project_root, "runs", "dl3p_pennfud", "deeplabv3_plus.h5")
-results_root = os.path.join(project_root, "results", "dl3p_pennfud")
+# results_root = os.path.join(project_root, "results", "dl3p_pennfud")
 
 
 """ Directory Creation """
@@ -127,8 +127,8 @@ if __name__ == "__main__":
 	model.load_weights(model_path)
 
 	""" Loading data """
-	# dataset_path = os.path.join(project_root, "data", "cityscapes", "new_data")
-	dataset_path = os.path.join(project_root, "data", "penn_fudan", "new_data")
+	dataset_path = os.path.join(project_root, "data", "cityscapes", "new_data")
+	# dataset_path = os.path.join(project_root, "data", "penn_fudan", "new_data")
 
 	print(f"Dataset path: {dataset_path}")
 
